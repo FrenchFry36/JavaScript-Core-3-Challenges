3684266ef2f5aa5a392ea03166a6fb59a266f300
Saidumar Akbarov <saidumarakbarov@Saidumars-MacBook-Air.local>
2025-01-20 16:44:43 +0100

    solved cowsay challenge

--- challenge-cowsay-two/solution2.js
+++ challenge-cowsay-two/solution2.js
@@ -1,18 +1,34 @@
 // =================
-// Stripped down cowsayer CLI, 
+// Stripped down cowsayer CLI,
 // no libraries or arguments
 // https://nodejs.dev/learn/accept-input-from-the-command-line-in-nodejs
 // =================
 
 // 1. Make  a command line interface.
-
 // 2. Make supplies for our speech bubble
+const saying = process.argv.slice(2).join(" ");
+const border = "=".repeat(saying.length);
+const CLInterface = `
+${border}
+${saying}
+${border}
+        \\    ^__^
+          \\  (oo)\\_______
+             (__)\\       )\\/\\
+                 ||----w |
+                 ||     ||
+`;
 
 // 3. Make a cow that takes a string
 
 const cow = (saying) => {
-    // how did you make the cow before?
-}
+  if (!saying) {
+    return "Please provide a message";
+  } else {
+    return CLInterface;
+  }
+};
 
-// 4. Use readline to get a string from the terminal 
+// 4. Use readline to get a string from the terminal
 // (with a prompt so it's clearer what we want)
+console.log(cow(saying));
